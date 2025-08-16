08b41127089549cdda3c1ddaf393509fc6e3648f
Dave Oliverio <davezer27@gmail.com>
2025-08-16 18:05:27 -0400

    maybe this will work

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
 
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit(),
+		 Icons({
+      		compiler: 'svelte',
+    }),
+	]
+};
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default config;
