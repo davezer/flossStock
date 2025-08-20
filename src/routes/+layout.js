b95de2a525050a449d6c6003422fb53934f6f4e9
Dave Oliverio <davezer27@gmail.com>
2025-08-20 15:32:06 -0400

    idk

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,12 +1,12 @@
 import { createBrowserClient, isBrowser } from '@supabase/ssr';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export async function load({ data, depends, fetch }) {
   depends('supabase:auth');
 
   // Only create the browser client in the browser.
   const supabase = isBrowser()
-    ? createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY, { global: { fetch } })
+    ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, { global: { fetch } })
     : null;
 
   // Reuse session/user calculated on the server
